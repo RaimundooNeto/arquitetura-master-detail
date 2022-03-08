83314e7c83f23460407b8dbda9ccf52d4f4bdd08
RaimundooNeto <raimundononatobatistaneto@gmail.com>
2022-03-08 10:49:18 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,15 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDataBase implements InMemoryDbService {
+  createDb() {
+    const categories = [
+      { id: 1, name: "Lazer", description: "Cinema, parques, praia, etc"},
+      { id: 2, name: "Moradia", description: "Pagamento de contas de casa"},
+      { id: 3, name: "Saúde", description: "Plano de saúde e Remédios"},
+      { id: 4, name: "Salário", description: "Recebimento de Salário"},
+      { id: 5, name: "Freelas", description: "Trabalhos como freelancer"}
+    ];
+
+    return { categories }
+  }
+}
