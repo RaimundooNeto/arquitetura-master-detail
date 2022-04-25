1e9c26b7f6e5449a984c5fbf84de83a6a195836d
RaimundooNeto <raimundononatobatistaneto@gmail.com>
2022-04-25 03:46:57 -0300

    project restruturing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataBase } from './in-memory-database';
-import { ToastrModule } from 'ngx-toastr';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase),
-    ToastrModule.forRoot()
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
