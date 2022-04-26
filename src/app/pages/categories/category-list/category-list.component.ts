576d5a69b6c4e7d709bb007cf2841e8b064af68d
RaimundooNeto <raimundononatobatistaneto@gmail.com>
2022-04-26 17:57:43 -0300

    improving CategoryList/categoruListComponent

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BaseResourceListComponent } from '../../../shared/components/base-resource-list/base-resource-list.component';
+
 import { Category } from '../shared/category.model';
 import { CategoryService } from '../shared/category.service';
 
@@ -8,30 +10,12 @@ import { CategoryService } from '../shared/category.service';
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
-
-  categories: Category[] = []
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
   constructor(
     private categoryService: CategoryService
-  ) { }
-
-  ngOnInit() {
-    this.categoryService.getAll().subscribe(
-      categories => this.categories = categories,
-      error => alert('Erro ao carregar a lista')
-    )
-  }
-
-  deletCategory(category: Category) {
-    const mustDelete = confirm("Deseja realmente excluir este item?");
-
-    if(mustDelete) {
-      this.categoryService.delete(category.id).subscribe(
-        () => this.categories = this.categories.filter(element => element != category),
-        () => alert("Erro ao tentar excluir")
-      )
-    }
+  ) {
+    super(categoryService);
   }
 
 }
